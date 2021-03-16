ebaac05034e395074a46fd3e2e52375194d02227
Leo Volpe <Leo Volpe@LAPTOP-VU9KPGVR.station>
2021-03-16 11:10:32 +0100

    finito es 2

--- Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
+++ Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -2,12 +2,15 @@ package it.polito.tdp.alien;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
+	
+	private AlienDictionary dizionario;
 
     @FXML
     private ResourceBundle resources;
@@ -16,25 +19,61 @@ public class FXMLController {
     private URL location;
     
     @FXML
+    private TextField txtInput;
+    
+    @FXML
     private Button btnTranslate;
+    
+    @FXML
+    private TextArea txtRis;
     
     @FXML
     private Button btnReset;
 
     @FXML
     void doTranslate(ActionEvent event) {
-    	// TODO - add the button and complete this    	
+    	 String testo= this.txtInput.getText();
+    	 if (testo.isEmpty()) {
+    		 this.txtRis.setText("Inserire una parola da cercare, o una parola e la sua traduzione");
+    		 return;
+    	 }
+    	 if (testo.contains("<")==false || testo.contains(">")==false) {
+    		 this.txtRis.setText("FORMATO SBAGLIATO, inserire <alienWord> <translate> o solo <alienWord>");
+    		 return;
+    	 }
+    	 String[] campi= testo.split("> <");
+    	 if (campi.length>2) {
+    		 this.txtRis.setText("Inserire max 2 stringhe: parola e traduzione");
+    		 return;
+    	 }
+    	 if(campi.length==1) {
+    		 String parolaAliena=campi[0].substring(1,campi[0].length()-1);
+    		 this.txtRis.setText("La traduzione della parola "+parolaAliena+" e': \n"+dizionario.transalteWord(parolaAliena));
+    	 } else {
+    		 String parolaAliena=campi[0].substring(1);
+    		 String traduzione=campi[1].substring(0, campi[1].length()-1);
+    		 this.dizionario.addWord(parolaAliena, traduzione);
+    		 this.txtRis.setText("Aggiunta la parola "+parolaAliena+" con la seguente traduzione: "+traduzione);
+    	 }
     }
     
     
     @FXML
     void doReset(ActionEvent event) {
-    	// TODO - add the button and complete this 
+    	this.txtInput.clear();
+    	this.txtRis.clear();
+    }
+    
+    public void setModel(AlienDictionary model) {
+    	this.dizionario=model;
     }
     
     
     @FXML
     void initialize() {
-
+    	 assert txtInput != null : "fx:id=\"txtInput\" was not injected: check your FXML file 'Scene.fxml'.";
+         assert btnTranslate != null : "fx:id=\"btnTranslate\" was not injected: check your FXML file 'Scene.fxml'.";
+         assert txtRis != null : "fx:id=\"txtRis\" was not injected: check your FXML file 'Scene.fxml'.";
+         assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
     }
 }
